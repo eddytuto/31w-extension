1baf91015ab1e27d0d323d232e9d1a1237221f60
Eddy Martin <eddypmartin@gmail.com>
2022-10-13 11:18:49 -0400

    c9 - ajout de css dans <style> temporaire

--- em_carrousel.php
+++ em_carrousel.php
@@ -9,7 +9,16 @@ Version: 1.0.0
 */
 
 function genere_boite(){
-    $contenu = "<div class='carrousel'>Carrousel</div>";
+    $contenu = "
+    <style>
+    .carrousel {
+        border: 2px solid #aaa;
+        background-color : #0f0;
+        width: 200px;
+        height : 200px;
+    }
+    </style>    
+    <div class='carrousel'>Voici le Carrousel</div>";
     return $contenu;
 }
 add_shortcode('em_carrousel', 'genere_boite');
